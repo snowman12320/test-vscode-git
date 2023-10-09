393c16709fa239cc88782ce00b5ff7c5832b3894
snowman12320 <snowman12320@gmail.com>
2023-10-09 16:55:00 +0800

    update 123.md on local 2

--- add.sh
+++ add.sh
@@ -3,7 +3,10 @@
 # git commit -m 'update 123.md on local 2'
 
 
-# local and remote edit together
+# local branch two and remote branch two edit together
+
+git pull
+
 git fetch origin
 git rebase origin/two
 
