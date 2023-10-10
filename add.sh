66be52ff5190e832cf1b02ece59c7655e6aab1ca
snowman12320 <snowman12320@gmail.com>
2023-10-10 14:44:30 +0800

    Add changes for branch 'three'

--- add.sh
+++ add.sh
@@ -12,27 +12,27 @@
 
 ### git push
 ## 1.
-git add .
-sleep 2
-git commit -m 'update add.sh'
-sleep 2
-git push
+# git add .
+# sleep 2
+# git commit -m 'update add.sh'
+# sleep 2
+# git push
 
 ### create branch and checkout,after modify,merge to main or master ,repair both modifyed,delete branch
 ## 1.
 # git checkout -b three
 # sleep 2
-# git add .
-# sleep 2
-# git commit -m "Add changes for branch 'three'"
-# sleep 2
+git add .
+sleep 2
+git commit -m "Add changes for branch 'three'"
+sleep 2
 ## Push the Branch to Remote (Optional):
 ## If you want to share your branch with others or keep it in a remote repository, you can push it to the remote repository using:
 # git push origin three
 ## 2.
-# git checkout main
-# sleep 2
-# git push origin main
+git checkout main
+sleep 2
+git push origin main
 
 
 
